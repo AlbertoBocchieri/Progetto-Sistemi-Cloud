80e30d4df7c92bdc0d303babab7ea4d2dbc8b743
Alberto Bocchieri <81562836+AlbertoBocchieri@users.noreply.github.com>
2026-07-22 20:08:34 +0200

    Aggiornata versione kubernetes da 1.30 a 1.35

--- infrastructure/terraform/aws/main.tf
+++ infrastructure/terraform/aws/main.tf
@@ -241,7 +241,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = local.name
-  cluster_version = "1.30"
+  cluster_version = "1.35"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
